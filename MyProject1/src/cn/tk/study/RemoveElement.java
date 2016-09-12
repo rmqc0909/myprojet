8171eda507babca27344f0ff6a90be5eb5dc753b
谢丹 <rmqc0909@163.com>
2016-09-12 11:28:26 +0800

    修改版本冲突的乱码

--- MyProject1/src/cn/tk/study/RemoveElement.java
+++ MyProject1/src/cn/tk/study/RemoveElement.java
@@ -36,7 +36,6 @@ public class RemoveElement {
 	}
 	public static void main(String[] args) 
 	{
-<<<<<<< HEAD
 		byte a[] = {1, 2, 4, 3, 2, 4, 7, 2};
 		System.out.println((byte)(-1) >>> 1);
 		System.out.println(Integer.signum(-1));
@@ -45,10 +44,8 @@ public class RemoveElement {
 		System.out.println(b);
 		//RemoveElement removeElement = new RemoveElement();
 		//removeElement.screenBlackNameList();
-=======
 		RemoveElement removeElement = new RemoveElement();
 		removeElement.screenBlackNameList();
->>>>>>> origin/master
 	}
 	
 	private void screenBlackNameList()
