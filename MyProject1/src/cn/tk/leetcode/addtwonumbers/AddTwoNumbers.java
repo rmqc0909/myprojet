3ce48562c83b67e780788594ccfc2396f13c9b6e
谢丹 <rmqc0909@163.com>
2016-09-12 13:23:28 +0800

    modify

--- MyProject1/src/cn/tk/leetcode/addtwonumbers/AddTwoNumbers.java
+++ MyProject1/src/cn/tk/leetcode/addtwonumbers/AddTwoNumbers.java
@@ -1,10 +1,10 @@
-package cn.tk.leetcode;
+package cn.tk.leetcode.addtwonumbers;
 
 /**
  * Created by xiedan on 16/8/22.
  *
- * You are given two linked lists representing two non-negative numbers. 
- * The digits are stored in reverse order and each of their nodes contain a single digit. 
+ * You are given two linked lists representing two non-negative numbers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
  * Add the two numbers and return it as a linked list.
 
  Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
@@ -12,18 +12,18 @@ package cn.tk.leetcode;
  */
 
 public class AddTwoNumbers {
-	/**
-	 * 
-	*
-	  recursion
-	* @param l1
-	* @param l2
-	* @return
-	 */
-	public static ListNode addTwoNumbersI(ListNode l1, ListNode l2) {
+    /**
+     *
+     *
+     recursion
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbersI(ListNode l1, ListNode l2) {
         return addLists(l1,l2,0);
     }
-    
+
     private static ListNode addLists(ListNode l1, ListNode l2, int carry) {
         if(l1 == null && l2 == null && carry == 0) return null;
         int digit = carry;
@@ -36,32 +36,32 @@ public class AddTwoNumbers {
         return head;
     }
     public static void main(String[] args) {
-    	ListNode l1 = new ListNode(4);
-    	l1.next = new ListNode(5);
-    	ListNode l2 = new ListNode(3);
-    	l2.next = new ListNode(8);
-		ListNode sumListI = addTwoNumbersI(l1, l2);
-		ListNode sumListII = addTwoNumbersII(l1, l2);
-		while(sumListI != null) {
-			System.out.println("sumListI value : " + sumListI.val);
-			sumListI = sumListI.next;
-		}
-		while(sumListII != null) {
-			System.out.println("sumListII value : " + sumListII.val);
-			sumListII = sumListII.next;
-		}
-	}
+        ListNode l1 = new ListNode(4);
+        l1.next = new ListNode(5);
+        ListNode l2 = new ListNode(3);
+        l2.next = new ListNode(8);
+        ListNode sumListI = addTwoNumbersI(l1, l2);
+        ListNode sumListII = addTwoNumbersII(l1, l2);
+        while(sumListI != null) {
+            System.out.println("sumListI value : " + sumListI.val);
+            sumListI = sumListI.next;
+        }
+        while(sumListII != null) {
+            System.out.println("sumListII value : " + sumListII.val);
+            sumListII = sumListII.next;
+        }
+    }
 
-	/**
-	*
-	  iterator
-	* @param l1
-	* @param l2
-	* @return
-	
-	*/
-	private static ListNode addTwoNumbersII(ListNode l1, ListNode l2) {
-		ListNode dummyHead = new ListNode(0);
+    /**
+     *
+     iterator
+     * @param l1
+     * @param l2
+     * @return
+
+     */
+    private static ListNode addTwoNumbersII(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
         ListNode cur = dummyHead;
         int carry = 0;
         while(l1 != null || l2 != null) {
@@ -79,7 +79,5 @@ public class AddTwoNumbers {
             cur.next = new ListNode(carry);
         }
         return dummyHead.next;
-	}
-    
-
+    }
 }
---
+++ MyProject1/src/cn/tk/leetcode/addtwonumbers/ListNode.java
@@ -0,0 +1,10 @@
+package cn.tk.leetcode.addtwonumbers;
+
+/**
+ * Created by xiedan on 16/8/22.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {val = x;}
+}
