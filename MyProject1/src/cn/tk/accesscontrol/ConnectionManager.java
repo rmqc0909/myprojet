c255ae9be8c2cf398382a5c205e6dff08e266099
谢丹 <rmqc0909@163.com>
2016-09-13 13:31:48 +0800

    modify

--- MyProject1/src/cn/tk/accesscontrol/ConnectionManager.java
+++ MyProject1/src/cn/tk/accesscontrol/ConnectionManager.java
@@ -1,8 +1,30 @@
 package cn.tk.accesscontrol;
 
-/**
+/**Think in Java Chapter vi - 8th   Page 123
  * Created by xiedan11 on 2016/9/12.
  */
-public class ConnectionManager {
+class Connection {
+    private Connection() {}
+    public static Connection makeConnection() {
+        return new Connection();
+    }
+}
 
+public class ConnectionManager {
+    private static int counter = 0;
+    private static Connection[]  pool = {Connection.makeConnection(), Connection.makeConnection(), Connection.makeConnection()};
+    public static Connection getConnection() {
+        if(counter < pool.length) {
+            System.out.println("get connection number: " + (counter + 1));
+            return pool[counter++];
+        }
+        return null;
+    }
+    public static void main(String args[]) {
+        Connection connection1 = ConnectionManager.getConnection();
+        Connection connection2 = ConnectionManager.getConnection();
+        Connection connection3 = ConnectionManager.getConnection();
+        Connection connection4 = ConnectionManager.getConnection();
+        System.out.println("connection1: " + connection1 + "|connection2: " + connection2 + "|connection3: " + connection3 + "|connection4: " + connection4);
+    }
 }
