e9ee881fa6a8b9280519f858f7a45c93fd5798da
谢丹 <rmqc0909@163.com>
2016-09-16 19:12:57 +0800

    modify

--- MyProject1/src/cn/tk/leetcode/TopKFrequentElements.java
+++ MyProject1/src/cn/tk/leetcode/TopKFrequentElements.java
@@ -5,6 +5,15 @@ import java.util.*;
 /**
  * Created by xiedan on 16/9/15.
  *
+ * Given a non-empty array of integers, return the k most frequent elements.
+
+ For example,
+ Given [1,1,1,2,2,3,3,4] and k = 2, return [1,2].
+
+ Note:
+ You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+ Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ *
  */
 public class TopKFrequentElements {
     public static void main(String args[]) {
