d35d3c45b22f58bed4d4e33766f0fef8f106b55e
rmqc0909 <rmqc0909@163.com>
2016-10-14 07:57:03 +0800

    fool

---
+++ MyProject1/src/cn/tk/sunpangzi/Mobile.java
@@ -0,0 +1,18 @@
+package cn.tk.sunpangzi;
+
+/**
+ * Created by xiedan on 16/10/14.
+ */
+
+public class Mobile {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 1, 7, 6, 3 ,8 ,9};
+        int[] index = {2, 0, 1, 1, 7, 5, 6, 2, 2, 3, 4};
+        String tel = "";
+        for (int x: index
+             ) {
+            tel += arr[x];
+        }
+        System.out.println(tel);
+    }
+}
