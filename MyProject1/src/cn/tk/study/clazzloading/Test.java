c9b629f32bff99c36fa1f2332b150cffc45d83bb
rmqc0909 <rmqc0909@163.com>
2016-11-14 21:41:15 +0800

    class_load

---
+++ MyProject1/src/cn/tk/study/clazzloading/Test.java
@@ -0,0 +1,15 @@
+package cn.tk.study.clazzloading;
+
+/**
+ * Created by xiedan on 2016/11/14.
+ */
+public class Test {
+    public static void main(String[] args) {
+        ClassLoader classLoader = Test.class.getClassLoader();
+        System.out.println(classLoader);
+        ClassLoader classLoader1 = classLoader.getParent();
+        System.out.println(classLoader1);
+        ClassLoader classLoader2 = classLoader1.getParent();
+        System.out.println(classLoader2);
+    }
+}
