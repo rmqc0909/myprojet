de1e652580657151df3e701504a6cadde7b66617
rmqc0909 <rmqc0909@163.com>
2016-11-15 22:05:44 +0800

    localcache

--- MyProject1/src/cn/tk/study/localcache/Calculate.java
+++ MyProject1/src/cn/tk/study/localcache/Calculate.java
@@ -8,24 +8,47 @@ import java.util.concurrent.ConcurrentHashMap;
  * java8和java7的本地缓存实现   重复操作需要加本地缓存以提高性能。
  */
 public class Calculate {
-    static Map<Integer, Integer> cache = new ConcurrentHashMap<>();
+    static Map<Integer, Integer> cache1 = new ConcurrentHashMap<>();
+    static Map<Integer, Integer> cache2 = new ConcurrentHashMap<>();
+    static Map<Integer, Integer> cache3 = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        for (int i = 0; i < 40; i++) {
-            System.out.println("f(" + i + ") = " + fibonacci(i));
+        long startTime1 = System.currentTimeMillis();
+        System.out.println("normal method:");
+        for (int i = 0; i < 45; i++) {
+            //System.out.println("f(" + i + ") = " + fibonacci(i));
+            fibonacci(i);
+        }
+        System.out.println("running time: " + (System.currentTimeMillis() - startTime1) + "ms");
+        long startTime2 = System.currentTimeMillis();
+        System.out.println("java7 synchronization method:");
+        for (int i = 0; i < 45; i++) {
             //System.out.println("f(" + i + ") = " + fibonacciJava7(i));
+            fibonacciJava7(i);
+        }
+        System.out.println("running time: " + (System.currentTimeMillis() - startTime2) + "ms");
+        long startTime3 = System.currentTimeMillis();
+        System.out.println("java8 computeIfAbsent method:");
+        for (int i = 0; i < 45; i++) {
             //System.out.println("f(" + i + ") = " + fibonacciJava8(i));
+            fibonacciJava8(i);
+        }
+        System.out.println("running time: " + (System.currentTimeMillis() - startTime3) + "ms");
+        long startTime4 = System.currentTimeMillis();
+        System.out.println("java7 method");
+        for (int i = 0; i < 45; i++) {
             //System.out.println("f(" + i + ") = " + fibonacciJava7I(i));
-       }
-       System.out.println("running time: " + (System.currentTimeMillis() - startTime) + "ms");
+            fibonacciJava7I(i);
+        }
+        System.out.println("running time: " + (System.currentTimeMillis() - startTime4) + "ms");
+
     }
 
 
     /**
      * 普通的实现方式
      * @param i
-     * @return
+     * @return  计算最终结果
      */
     private static int fibonacci(int i) {
         if (i == 0 || i == 1)
@@ -37,29 +60,29 @@ public class Calculate {
     /**
      *java8的computeIfAbsent方法可以在key所对应的value值不存在的情况下，计算一个新的value值。
      * @param i
-     * @return
+     * @return    计算最终结果
      */
     private static int fibonacciJava8(int i) {
         if (i == 0 || i == 1)
             return i;
-        return cache.computeIfAbsent(i, (key) -> fibonacciJava8(i - 2) + fibonacciJava8(i - 1));
+        return cache2.computeIfAbsent(i, (key) -> fibonacciJava8(i - 2) + fibonacciJava8(i - 1));
     }
 
     /**
      * java7 通过synchronized进行线程同步，检查缓存是否存在key对应的值，如果不存在才进行相应的计算。
      * @param i
-     * @return
+     * @return   计算最终结果
      */
     private static int fibonacciJava7(int i) {
         if (i == 0 || i == 1)
             return i;
-        Integer result = cache.get(i);
+        Integer result = cache1.get(i);
         if (result == null) {
-            synchronized (cache) {
-                result = cache.get(i);
+            synchronized (cache1) {
+                result = cache1.get(i);
                 if (result == null) {
                     result = fibonacciJava7(i - 2) + fibonacciJava7(i - 1);
-                    cache.put(i, result);
+                    cache1.put(i, result);
                 }
             }
         }
@@ -69,15 +92,15 @@ public class Calculate {
     /**
      * java7 检查缓存是否存在key对应的值，如果不存在才进行相应的计算 （不进行同步控制）
      * @param i
-     * @return
+     * @return   计算最终结果
      */
     private static int fibonacciJava7I(int i) {
         if (i == 0 || i == 1)
             return i;
-        Integer result = cache.get(i);
+        Integer result = cache3.get(i);
         if (result == null) {
             result = fibonacciJava7(i - 2) + fibonacciJava7(i - 1);
-            cache.put(i, result);
+            cache3.put(i, result);
         }
         return result;
     }
