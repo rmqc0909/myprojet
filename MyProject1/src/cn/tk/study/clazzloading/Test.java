0ec9badcadd6932e601505176f4c4bdc9163fd96
谢丹 <rmqc0909@163.com>
2016-11-15 17:29:53 +0800

    modify clazzloading

--- MyProject1/src/cn/tk/study/clazzloading/Test.java
+++ MyProject1/src/cn/tk/study/clazzloading/Test.java
@@ -6,12 +6,12 @@ package cn.tk.study.clazzloading;
 public class Test {
     public static void main(String[] args) {
         ClassLoader classLoader = Test.class.getClassLoader();
-        System.out.println(classLoader);
+        System.out.println("Test classloader: " + classLoader);
         ClassLoader classLoader1 = classLoader.getParent();
-        System.out.println(classLoader1);
+        System.out.println("Test parent classloader: " + classLoader1);
         ClassLoader classLoader2 = classLoader1.getParent();
-        System.out.println(classLoader2);
+        System.out.println("Test grandparent classloader: " + classLoader2);
         ClassLoader classLoader3 = Integer.class.getClassLoader();
-        System.out.println("Integer:" + classLoader3);
+        System.out.println("Integer classloader: " + classLoader3);
     }
 }
